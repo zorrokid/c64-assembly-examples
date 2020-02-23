1a0a4b4241bb1523bd7d01c61d00ddeeacbd6984
Mikko Keinänen <mikko.keinanen@gmail.com>
2020-02-23 20:26:32 +0200

    Cleaned up the example

--- 04-print_character_set/main.asm
+++ 04-print_character_set/main.asm
@@ -1,32 +1,34 @@
-; we're using zero-page vector (pair of bytes) for indirect-indexed access to screen memory:
-tmp2lo  = $37
-tmp2hi  = $38
+; we're using zero-page vector (pair of bytes) for indirect-indexed access to screen memory
+; using some unused addresses here (zero-page memory map: https://www.c64-wiki.com/wiki/Zeropage)
+tmp_lo  = $fb
+tmp_hi  = $fc
 
 	; Start memory location for program
 	*=$0801
 
 	jsr print_char_set
 
-loop: 	; just a wait loop	
-	jmp  loop
+; just a wait loop: using acme-assembler anonymous backward label feature here ('-'-label)
+- 	
+	jmp - 	; when referencing '-'-label, it refers to previous definition of '-'-label
 
-print_char_set:
+print_char_set
 
-	; store start of screen memory address $0400 to zero page vector 
-	; low byte:
+	; Store start of screen memory address $0400 to zero page vector 
+	; low byte...:
 	lda #<$0400
-	sta tmp2lo
-	; high byte:
+	sta tmp_lo
+	; ... and high byte:
 	lda #>$0400
-	sta tmp2hi
+	sta tmp_hi
 
 	; copy all 255 characters to screen memory
-	; y is used for both referencing the character value and as an index for screen memory location
+	; y is used for both referencing the character value and as an index to screen memory location
 	ldy #$00
-.copyloop
+-	
 	tya
-	sta (tmp2lo),y
+	sta (tmp_lo),y
 	iny
-	bne .copyloop 	; loop until y has 0 again
+	bne - 	; loop until y has 0 again
 
 	rts
